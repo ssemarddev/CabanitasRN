c6d1c4c7e690d9ba014b669044fa80a279976076
LuisVazquezPerez <luiscarlosvazquezperez@gmail.com>
2025-02-06 20:44:51 -0600

    INICIO

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "../global.css"
+
+const _layout = () => {
+  return (
+    <View>
+      <Text className='text-blue-200 bg-red-600'>_layout</Text>
+    </View>
+  )
+}
+
+export default _layout
